d10050a5b34f7308086cbdcac7f00d21f0a3de26
vdelgado1407 <vdelgado.vd86@gmail.com>
2020-02-03 11:40:30 -0600

    Day 1

--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var itemTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func whenButtonPressed(_ sender: Any) {
+    }
+    
 }
 
