a518f5c70696ac5eebab826c361fe9c5ef357ea0
vdelgado1407 <vdelgado.vd86@gmail.com>
2020-02-04 11:18:50 -0600

    Day 2

---
+++ ShoppingList/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  ShoppingList
+//
+//  Created by Victor Delgado on 2/4/20.
+//  Copyright © 2020 John Hersey High School. All rights reserved.
+//
+
+import Foundation
+
+class item {
+    var name:String
+    
+    init(name:String) {
+        self.name = name
+    }
+}
--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -8,17 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
     @IBOutlet weak var itemTextField: UITextField!
     @IBOutlet weak var tableView: UITableView!
+    var items:[item] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        
+        let item1 = item(name: "Milk")
+        let item2 = item(name: "Eggs")
+        
+        items = [item1,item2]
     }
 
     @IBAction func whenButtonPressed(_ sender: Any) {
+        
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        cell.textLabel?.text = "Hello"
+        return cell
+    }
 }
 
